e1097659833b0748a618e42585e879227a7630a2
JamesDeegan <jamesdeegan@hotmail.co.za>
2018-11-20 17:54:07 +0100

    Update vuedraggable.js

--- dist/vuedraggable.js
+++ dist/vuedraggable.js
@@ -188,9 +188,9 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
       computed: {
         rootContainer: function rootContainer() {
-          console.log(this.$el)
+          console.log(this.$el.children[0])
           console.log('- - - - - - - ')
-          return this.transitionMode ? this.$el.children[0] : this.$el;
+          return this.transitionMode ? this.$el.children[0].children[0] : this.$el.children[0];
         },
         isCloning: function isCloning() {
           return !!this.options && !!this.options.group && this.options.group.pull === 'clone';
